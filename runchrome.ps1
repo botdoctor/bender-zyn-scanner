177d82b9c02d09da157d16869cce834226dcffef
botdoctor <92708652+botdoctor@users.noreply.github.com>
2025-05-27 17:30:27 -0500

    Update runchrome.ps1

--- runchrome.ps1
+++ runchrome.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--remote-debugging-port=9222", "--user-data-dir=C:\Users\Bendersec\ChromeDebugProfile"
+Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--remote-debugging-port=9222", "--user-data-dir=C:\Users\Username\ChromeDebugProfile"
